d6b3df22c1f939c77bf8b5610da8f581fd20e39c
Dav991123 <sargsyand89@gmail.com>
2024-07-06 18:10:42 +0400

    add header global component and implement notifications after register

--- src/view/pages/auth/register/index.js
+++ src/view/pages/auth/register/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography, Input, Button, Divider } from 'antd';
+import { Typography, Input, Button, Divider, Form, notification } from 'antd';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth, app } from '../../../../services/firebase/firebase';
 
@@ -11,90 +11,115 @@ class Register extends React.Component {
     constructor() {
         super();
         this.state = {
+            loading: false,
             firstName: '',
             lastName: '',
-            headline: '',
             email: '',
-            password: ''
+            password: '',
+            headline: ''
         }
 
         this.handleRegister = this.handleRegister.bind(this);
     }
 
-    handleChangeInput = e => {
-        const { name, value } = e.target;
-        this.setState({
-            [name]: value
-        });
+    handleChangeInput = value => {
+       this.setState(value)
     }
     
-    handleRegister() {
-        const { email, password } = this.state; 
-        createUserWithEmailAndPassword(auth, email, password);
+    async handleRegister() {
+        const { email, password, firstName, lastName } = this.state; 
+        this.setState({
+            loading: true
+        });
+
+       try{
+        const response = await createUserWithEmailAndPassword(auth, email, password);
+        console.log(response);
+        notification.success({
+            message: 'Success Registration',
+            description: `Welcome dear ${firstName} ${lastName}`
+        })
+       } catch(error) {
+            notification.error({
+                message: 'Wrong Registration',
+                description: `Ooooops :(`
+            })
+       } finally {
+            this.setState({
+                loading: false
+            });
+       }
     }
 
-    render() {
 
+
+    render() {
         return (
             <div className="auth_register_container">
                 <Title level={2}>
                     Register
                 </Title>
 
+                <Form layout='vertical' onValuesChange={this.handleChangeInput}>
+                    <Form.Item label="First Name" name="firstName">
+                        <Input 
+                            type="text"
+                            placeholder="First Name"
+                        />
+                    </Form.Item>
 
-                <div>
-                    <Input 
-                        type="text"
-                        name="firstName"
-                        placeholder="First Name"
-                        onChange={this.handleChangeInput}
-                    />
-                </div>
+                    <Form.Item label="Last Name" name="lastName">
+                        <Input 
+                            type="text"
+                            placeholder="Last Name"
+                        />
+                    </Form.Item>
 
-                <div>
-                    <Input 
-                        type="text"
-                        name="lastName"
-                        placeholder="Last Name"
-                        onChange={this.handleChangeInput}
-                    />
-                </div>
+                    <Form.Item label="Headline" name="headline">
+                        <Input 
+                            type="text"
+                            placeholder="Headline"
+                        />
+                    </Form.Item>
 
-                <div>
-                    <Input 
-                        type="text"
-                        name="headline"
-                        placeholder="Headline"
-                        onChange={this.handleChangeInput}
-                    />
-                </div>
+                    <Form.Item label="Email" name="email">
+                        <Input 
+                            type="email"
+                            placeholder="Email"
+                        />
+                    </Form.Item>
 
-                <div>
-                    <Input 
-                        type="email"
-                        name="email"
-                        placeholder="Email"
-                        onChange={this.handleChangeInput}
-                    />
-                </div>
+                    <Form.Item label="Password" name="password">
+                        <Input 
+                            type="password"
+                            placeholder="Password"
+                        />
+                    </Form.Item>
 
-                <div>
-                    <Input 
-                        type="password"
-                        name="password"
-                        placeholder="Password"
-                        onChange={this.handleChangeInput}
-                    />
-                </div>
+                    <Divider />
 
-                <Divider />
+                    <Button
+                        type="primary" 
+                        onClick={this.handleRegister}
+                        loading={this.state.loading}
+                    >
+                        Register
+                    </Button>
+                </Form>
+               
 
-                <Button type="primary" onClick={this.handleRegister}>
-                    Register
-                </Button>
             </div>
         )
     }
 }
 
 export default Register;
+
+
+
+
+
+
+
+
+
