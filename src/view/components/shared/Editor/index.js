56a064fa9e6fc084d5f575b24e5d92746c94f3ed
Dav991123 <sargsyand89@gmail.com>
2024-07-29 22:20:29 +0400

    implement editor text

--- src/view/components/shared/Editor/index.js
+++ src/view/components/shared/Editor/index.js
@@ -2,14 +2,15 @@ import { Editor as EditorTinymce } from '@tinymce/tinymce-react';
 
 const Editor = ({ value, onChange }) => {
 
-    const handleChange = () => {
-
+    const handleChange = (editorValue) => {
+        onChange(editorValue);
     };
     
     return (
         <EditorTinymce
             onEditorChange={handleChange}
             apiKey="j13ks5nfta7dgljey2ui0nyxdmna6iv6uk9jea5zfntp26vw"
+            value={value}
             init={{
             height: 300,
             menubar: false,
